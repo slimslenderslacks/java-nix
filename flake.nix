9ba95fd6cc7dd224e68cdc4e78ae71250cb7d347
Jim Clark <slimslenderslacks@gmail.com>
2023-05-12 18:19:50 -0700

    initial try

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    mavenix.url = "github:nix-community/mavenix";
+  };
+
+  outputs = { nixpkgs, flake-utils, mavenix, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {inherit system;};
+	maven = with pkgs; (buildMaven ./project-info.json);
+	cli = import mavenix; 
+      in
+      rec {
+        packages.repo = maven.repo;
+	packages.build = maven.build;
+        packages.create-project-info = pkgs.writeShellScriptBin "app" ''
+          ${pkgs.maven}/bin/mvn org.nixos.mvn2nix:mvn2nix-maven-plugin:mvn2nix 
+        '';
+
+        devShells.default = pkgs.mkShell
+          {
+            packages = [
+              pkgs.maven
+	      cli.defaultPackage
+            ];
+          };
+      });
+}
