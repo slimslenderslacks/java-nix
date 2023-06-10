56e2879aa170a89e9da0af77a0816c72adda0cfe
Jim Clark <slimslenderslacks@gmail.com>
2023-06-09 17:22:13 -0700

    add libstdc++ to LD_LIBRARY_PATH

--- flake.nix
+++ flake.nix
@@ -13,11 +13,16 @@
 	src = dirOf ./project-info.json;
       in
       rec {
+
+        # package to represent a local .m2 repo
         packages.repo = maven.repo;
+
+	# package to run the maven plugin that will transform pom.xml to a project-info.xml
         packages.create-project-info = pkgs.writeShellScriptBin "app" ''
           ${pkgs.maven}/bin/mvn org.nixos.mvn2nix:mvn2nix-maven-plugin:mvn2nix 
         '';
 	
+	# package to create the jars for this app
         packages.jars = pkgs.stdenv.mkDerivation {
           name = "${maven.info.project.artifactId}-${maven.info.project.version}";
 
@@ -27,7 +32,7 @@
               != (toString (src + "/.git")))
             src;
 
-          buildInputs = [ pkgs.maven ];
+          buildInputs = [ pkgs.maven pkgs.gcc-unwrapped.lib ];
 
           buildPhase = "mvn --offline --settings ${maven.settings} compile";
 
@@ -38,8 +43,10 @@
           '';
         };
      
+        # default package is an entrypoint to run the jars
         packages.default = pkgs.writeShellScriptBin "entrypoint" ''
-	  ${pkgs.temurin-bin}/bin/java -cp ${packages.jars}/lib/deeplearning4j-example-sample-1.0.0-M2-bin.jar org.deeplearning4j.examples.sample.LeNetMNIST
+export LD_LIBRARY_PATH=${pkgs.gcc-unwrapped.lib}/lib64
+${pkgs.temurin-bin}/bin/java -cp ${packages.jars}/lib/deeplearning4j-example-sample-1.0.0-M2-bin.jar org.deeplearning4j.examples.sample.LeNetMNIST
 	'';
 
         devShells.default = pkgs.mkShell
